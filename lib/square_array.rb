27b0d91b41547e86bba417617d1e27c134b86a6d
charming-invocation-5395 <evaldman@yahoo.com>
2020-11-14 22:36:44 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,9 @@ def square_array(array)
   counter = 0 
   new_array = []
 
-  while counter < do
-    
-  
+  while counter < array.length do
+    new_array.push(array[counter] * array[counter])
+    coutner += 1 
+  end
+  new_array
 end
